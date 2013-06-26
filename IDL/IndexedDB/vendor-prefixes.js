0d7c7fbc6d4759a7093d6c1711b8ca42450569c9
Jens Arps <mail@jensarps.de>
2013-06-26 13:48:02 +0200

    Add vendor prefixes file

---
+++ IDL/IndexedDB/vendor-prefixes.js
@@ -0,0 +1,10 @@
+/*
+  These vendor prefixed references are not part of any specification, but
+  exist just to avoid errors triggered by static analysis tools like JSHint.
+ */
+
+webkitIndexedDB = indexedDB;
+webkitIDBKeyRange = IDBKeyRange;
+
+mozIndexedDB = indexedDB;
+mozIDBKeyRange = IDBKeyRange;
